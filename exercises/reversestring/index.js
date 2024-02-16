932cab6aaa643cd2c65b1de2c8448cd38104fcbe
Jina2ee <jina2ee1011@gmail.com>
2024-02-16 23:27:45 +0000

    Solve Reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split("").reverse().join("")
+}
 
-module.exports = reverse;
+// // #1 solution
+// function reverse(str) {
+//   return str.split("").reverse().join("")
+// }
+// #2 solution
+// function reverse(str) {
+//   let reversed = ""
+
+//   for (const character of str) {
+//     reversed = character + reversed
+//     // debugger
+//   }
+//   return reversed
+// }
+
+// #3 solution
+// function reverse(str) {
+//   // debugger
+//   return str.split("").reduce((rev, char) => char + rev, "")
+// }
+
+// reverse("asdf")
+module.exports = reverse
+
+// debugger
+// node inspect filename
+// c : continue
+// repl
+// possible testing your data inside function
