d3cd8f4dc29fba1333c89d21961b06c68c00b486
Jina2ee <jina2ee1011@gmail.com>
2024-02-23 11:53:37 +0000

    Solve MaxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//   let object = {}
+//   let max = 0
+//   let key = ""
+//   str.split("").forEach((char) => {
+//     object[char] = object[char] + 1 || 1
+//   })
 
-module.exports = maxChar;
+//   for (const char in object) {
+//     if (object[char] > max) {
+//       max = object[char]
+//       key = char
+//     }
+//   }
+//   return key
+// }
+
+module.exports = maxChar
+
+// #1 solution
+function maxChar(str) {
+  const charMap = {}
+  let max = 0
+  let maxChar = ""
+
+  // for array or string => for of
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  }
+
+  for (const char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+  return maxChar
+}
