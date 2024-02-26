c7dcb1867e2970be53625dfb41a0f9574d6b9616
Jina2ee <jina2ee1011@gmail.com>
2024-02-26 18:39:09 +0000

    Solve Capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,36 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//   const capStr = str.split(" ").map((st) => {
+//     return st[0].toUpperCase() + st.slice(1)
+//   })
+//   return capStr.join(" ")
+// }
 
-module.exports = capitalize;
+// #1 solution
+// function capitalize(str) {
+//   const words = []
+
+//   for (let word of str.split(" ")) {
+//     words.push(word[0].toUpperCase() + word.slice(1))
+//   }
+
+//   return words.join(" ")
+// }
+
+//  #2 solution
+function capitalize(str) {
+  let result = str[0].toUpperCase()
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
+  }
+  return result
+}
+
+module.exports = capitalize
