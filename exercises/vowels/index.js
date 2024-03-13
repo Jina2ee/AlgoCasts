7f59bac97f19f536621465ae0439f84a1a2779ff
Jina2ee <jina2ee1011@gmail.com>
2024-03-13 21:03:45 +0000

    Solve Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,38 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   let value = 0
+//   for (const key of str.toUpperCase().split("")) {
+//     if (
+//       key === "A" ||
+//       key === "E" ||
+//       key === "I" ||
+//       key === "O" ||
+//       key === "U"
+//     ) {
+//       value += 1
+//     }
+//   }
+//   return value
+// }
 
-module.exports = vowels;
+// # 1 solution
+// function vowels(str) {
+//   let count = 0
+//   let checker = ["a", "e", "i", "o", "u"]
+
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++
+//     }
+//   }
+//   return count
+// }
+// # 2 solution
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
+}
+
+module.exports = vowels
